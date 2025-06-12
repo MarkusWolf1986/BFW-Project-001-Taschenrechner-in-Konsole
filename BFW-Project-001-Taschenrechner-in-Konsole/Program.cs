d9740b88bfb69094c4680aebe37713c08975edd8
Markus Wolf <mobiletoolbox0@gmail.com>
2025-06-12 14:37:11 +0200

    Projektdateien hinzufügen.

---
+++ BFW-Project-001-Taschenrechner-in-Konsole/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace BFW_Project_001_Taschenrechner_in_Konsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+
+
+
+
+
+        }
+    }
+}
