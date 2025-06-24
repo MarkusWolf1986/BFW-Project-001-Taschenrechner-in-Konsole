dbf656304e5cb34bf96e06d02903ba97ac7b6ff4
Markus Wolf <mobiletoolbox0@gmail.com>
2025-06-24 09:39:04 +0200

    Kaffee holen

--- BFW-Project-001-Taschenrechner-in-Konsole/Program.cs
+++ BFW-Project-001-Taschenrechner-in-Konsole/Program.cs
@@ -1,17 +1,86 @@
-﻿namespace BFW_Project_001_Taschenrechner_in_Konsole
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace BFW_Project_001_Taschenrechner_in_Konsole
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            // Globale Variablen
+            bool repeetProgram = true;
+
+            // Whileschleife, um das Programm mehrmals durchlaufen
+            while (repeetProgram = true)
+            {
+
+                // Variable für das hauptsächliche Programm
+
+                // Die beiden Zahlen mit denen wir rechen
+                double zahl1 = 0;
+                double zahl2 = 0;
+
+                // Der Rechenoperator
+                string calculationOperator = null;
+
+
+
+                // Der Programmablauf
+
+                // Den User zu Eingabe der ersten Zahl auffordern
+                Console.WriteLine("Bitte geben Sie die erste Zahle ein.");
+
+                // Die Zahl 1 vom User entgegennehmen
+                Console.ReadLine();
+
+
+
+                // Den User zu Eingabe des Rechenoperators auffordern
+                Console.WriteLine("Bitte geben Sie die zweite Zahle ein.");
+
+                // Den User zu Eingabe des Rechenoperators auffordern
+                Console.ReadLine();
+
+
+
+                // Den User zu Eingabe der zweiten Zahl auffordern
+                Console.WriteLine("Bitte geben Sie den Rechenoperator ein.");
+
+                // Die Zahl 2 vom User entgegennehmen
+                Console.ReadLine();
 
 
 
 
+            }
 
 
 
+        // Statische Methoden für die Main
+
+
+        // Methode, um die Zahl 1 entgegenzunehmen
+        public static int getNumber1
+        {
+
+            return getNumber1;
         }
+
+        // Methode, um die Zahl 2 entgegenzunehmen
+        public static int getNumber2
+        {
+
+            return getNumber2;
+        }
+
+        // Get Rechenoperator
+        public static int getNumber2
+        {
+
+            return getNumber2;
+        }
+
+
     }
 }
